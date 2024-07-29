8a8347435148247064986764eb3f092b23a41032
Bapat <P70074418@unimaas.nl>
2024-07-29 14:52:46 +0530

    tokens generated for login

--- gatekeeper/views/AuthV.py
+++ gatekeeper/views/AuthV.py
@@ -1,15 +1,34 @@
+import jwt
+import logging
+import os
+from datetime import datetime, timedelta
+
 from django.views.generic import TemplateView
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.views.decorators.cache import never_cache
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
 from django.urls import reverse, resolve, Resolver404
-import logging
+from django.http import HttpResponseRedirect
 
 from gatekeeper.forms import LoginForm, RegisterForm, PasswordResetForm
 from aegis.models import DefaultAuthUserExtend
 
 logger = logging.getLogger('aegis')
+
+# Secret keys for JWT encoding
+ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET', 'default_access_token_secret')
+REFRESH_TOKEN_SECRET = os.getenv('REFRESH_TOKEN_SECRET', 'default_refresh_token_secret')
+JWT_SECRET = os.getenv('JWT_SECRET', 'default_jwt_secret')
+
+print("ACCESS_TOKEN_SECRET:", ACCESS_TOKEN_SECRET)
+print("REFRESH_TOKEN_SECRET:", REFRESH_TOKEN_SECRET)
+print("JWT_SECRET:", JWT_SECRET)
+
+# Token expiration times
+ACCESS_TOKEN_EXPIRATION = timedelta(minutes=60)
+REFRESH_TOKEN_EXPIRATION = timedelta(days=1)
+JWT_EXPIRATION = timedelta(hours=1)
 
 
 @method_decorator(never_cache, name='dispatch')
@@ -39,13 +58,32 @@ class LoginView(TemplateView):
         if form.is_valid():
             user = form.get_user()
             auth_login(request, user)
+
+            # Generate tokens
+            access_token = self.generate_token(user.id, ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRATION)
+            refresh_token = self.generate_token(user.id, REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRATION)
+            jwt_token = self.generate_token(user.id, JWT_SECRET, JWT_EXPIRATION)
+
+            print(access_token)
+            print(refresh_token)
+            print(jwt_token)
+
+            # Create response
+            if 'farm_calendar' in next_url:
+                response = HttpResponseRedirect('http://127.0.0.1:8002/test_perm')
+            else:
+                response = HttpResponseRedirect(next_url or 'aegis:dashboard')
+
+            # Set cookies
+            response.set_cookie('access_token', access_token, httponly=True)
+            response.set_cookie('refresh_token', refresh_token, httponly=True)
+            response.set_cookie('jwt', jwt_token, httponly=True)
+
+            # Clear the next URL from session after successful login
             if next_url:
-                del request.session['next']  # Clear the next URL from session after successful login
-                if self.is_valid_url(next_url):
-                    return redirect(next_url)
-                elif 'farm_calendar' in next_url:
-                    return redirect('http://127.0.0.1:8002')
-            return redirect('aegis:dashboard')
+                del request.session['next']
+
+            return response
         context = self.get_context_data(**kwargs)
         context['form'] = form
         context['next'] = next_url
@@ -64,6 +102,17 @@ class LoginView(TemplateView):
             return True
         except Resolver404:
             return False
+
+    def generate_token(self, user_id, secret, expiration):
+        """
+        Generate a JWT token.
+        """
+        payload = {
+            'user_id': user_id,
+            'exp': datetime.utcnow() + expiration,
+            'iat': datetime.utcnow()
+        }
+        return jwt.encode(payload, secret, algorithm='HS256')
 
 
 @method_decorator(never_cache, name='dispatch')
