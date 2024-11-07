1b1630209de213f317233dfdfa1364c756790e19
Pranav Bapat <pranavnbapat@users.noreply.github.com>
2024-11-07 23:34:48 +0100

    mysql pass changed

--- mysql_docker/sql_scripts/01_init.sql
+++ mysql_docker/sql_scripts/01_init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS gatekeeper;
 ALTER DATABASE gatekeeper CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL PRIVILEGES ON gatekeeper.* TO 'gatekeeper_admin'@'%' IDENTIFIED BY '`~G0Q3,Ju#';
+GRANT ALL PRIVILEGES ON gatekeeper.* TO 'gatekeeper_admin'@'%' IDENTIFIED BY 'asdasdasd';
 FLUSH PRIVILEGES;
