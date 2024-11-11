1d9403d156f57a404c4fd60d5cca35da77c4e982
Pranav Bapat <pranavnbapat@users.noreply.github.com>
2024-11-11 22:31:48 +0100

    waitress improved

--- run_waitress.py
+++ run_waitress.py
@@ -6,10 +6,13 @@ from waitress import serve
 from gatekeeper.wsgi import application
 
 host = os.getenv('APP_HOST', '0.0.0.0')
-port = int(os.getenv('APP_PORT', '9000'))
+port = int(os.getenv('APP_PORT', '8001'))
 
 logging.basicConfig(filename='logs/waitress.log', level=logging.INFO)
 
 warnings.filterwarnings("ignore")
+
+# Print the binding information for confirmation
+print(f"Serving Django application on http://{host}:{port}")
 
 serve(application, host=host, port=port)
